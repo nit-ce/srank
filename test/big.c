4630920b00973282ae20d73770cf25c8a73d4252
Ali Gholami Rudi <ali@rudi.ir>
2017-07-12 14:36:25 +0430

    test: multiple preferences in the big.c test

--- test/big.c
+++ test/big.c
@@ -22,17 +22,24 @@ static void minors(void)
 {
 	int i;
 	char name[128];
+	char req[128];
 	for (i = 0; i < NMINOR; i++) {
 		sprintf(name, MNAME, i);
 		printf("minor\t%s\n", name);
 		printf("minor_msc\t%s\t%d\n", name, 5 + i % 7);
-		printf("minor_req\t%s\t%s\n", name, name);
+		sprintf(req, MNAME, (i + 0) % NMINOR);
+		printf("minor_req\t%s\t%s\n", name, req);
+		sprintf(req, MNAME, (i + NMINOR - 1) % NMINOR);
+		printf("minor_req\t%s\t%s\n", name, req);
+		sprintf(req, MNAME, (i + NMINOR - 2) % NMINOR);
+		printf("minor_req\t%s\t%s\n", name, req);
 	}
 }
 
 static void students(void)
 {
 	char name[128];
+	char pref[128];
 	char uname[128];
 	char mname[128];
 	int i;
@@ -41,10 +48,16 @@ static void students(void)
 		sprintf(uname, UNAME, i % NUNIV);
 		sprintf(mname, MNAME, i % NMINOR);
 		printf("student\t%s\n", name);
+		printf("student_name\t%s\t%s\t%s\n", name, name, name);
 		printf("student_bsc\t%s\t%s\n", name, mname);
 		printf("student_bscuni\t%s\t%s\n", name, uname);
 		printf("student_bscgpa\t%s\t%d.%02d\n", name, 16 + i % 4, i % 100);
-		printf("student_pref\t%s\t%s\n", name, mname);
+		sprintf(pref, MNAME, (i + 0) % NMINOR);
+		printf("student_pref\t%s\t%s\n", name, pref);
+		sprintf(pref, MNAME, (i + 1) % NMINOR);
+		printf("student_pref\t%s\t%s\n", name, pref);
+		sprintf(pref, MNAME, (i + 2) % NMINOR);
+		printf("student_pref\t%s\t%s\n", name, pref);
 	}
 }
 
